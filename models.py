70d77f6f41bbc5ec3e8900cdb93e8259a345044b
kemalcan bora <kemalcanbora@gmail.com>
2022-11-23 13:59:01 +0100

    Update models.py

--- models.py
+++ models.py
@@ -8,15 +8,13 @@ class Recipe:
     category: str
     calories: float
     ingredients: List
-    instructions: List
 
     def dict(self):
         return {
             "name": self.name,
             "category": self.category,
             "calories": self.calories,
-            "ingredients": self.ingredients,
-            "instructions": self.instructions
+            "ingredients": self.ingredients
         }
 
 
@@ -33,16 +31,4 @@ class Ingredients:
             "name": self.name,
             "amount": self.amount,
             "unit": self.unit
-        }
-
-
-@dataclass
-class Instruction:
-    definition: str
-    index: int
-
-    def dict(self):
-        return {
-            "definition": self.definition,
-            "index": self.index
         }
